a5d95d25f5eb770234ae13e1bf267936267755dc
f-birne <122304597+f-birne@users.noreply.github.com>
2025-11-28 11:32:12 +0100

    corrected previous commit

--- src/api/GenerateImage.js
+++ src/api/GenerateImage.js
@@ -1,15 +1,14 @@
-import FormData from "form-data";
-
-const form = new FormData();
-form.append("text", prompt);
-
-const response = await fetch("https://api.deepai.org/api/text2img", 
+export async function GenerateImage(word) 
 {
-    method: "POST",
-    headers: 
+    const response = await fetch("https://rainfall-words-server-production.up.railway.app/generate-image", 
     {
-        "api-key": process.env.DEEPAI_KEY
-    },
-    body: form
-    
-});
+        method: "POST",
+        headers: 
+        {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ word })
+    });
+    const data = await response.json();
+    return data;
+}
